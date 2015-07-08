ce5399c7e098fec5783ef9eb2526c064f5bb26e0
humblenut <austnig94@gmail.com>
2015-07-07 23:07:24 -0500

    chart now auto adjusts the lower range so not stuck at zero changed tital of chart and scene removed the legend added comments to DrawChart class

--- src/DrawChart.java
+++ src/DrawChart.java
@@ -10,64 +10,87 @@ import static thorwin.math.Math.polyfit;
 import static thorwin.math.Math.polynomial;
 
 
+@SuppressWarnings("unchecked")
 public class DrawChart extends Application {
 
-    @Override
-    @SuppressWarnings("unchecked")
+//    @Override
+//    @SuppressWarnings("unchecked")
     public void start(Stage stage) throws Exception {
+
+        //Parses data from csv file
         CsvFileReader csv = new CsvFileReader();
         csv.readCSV();
+
+        //line chart title
         stage.setTitle("TypeRacer - Chart");
+
+        //arrays for xy values
         double[] xs = new double[csv.raceDB.size()];
         double[] ys = new double[csv.raceDB.size()];
 
-        //Data points
+        //create xy axis
         final NumberAxis xAxis = new NumberAxis();
         final NumberAxis yAxis = new NumberAxis();
+
+        //axis labels
         xAxis.setLabel("Race Number");
         yAxis.setLabel("WPM");
 
-
-
-
+        //create line chart
         final LineChart<Number, Number> lineChart = new LineChart<Number, Number>(xAxis,yAxis);
+
+        //series1 scatter style data points
         XYChart.Series<Number, Number> series1 = new XYChart.Series();
+
+        //series2 polynominal curve data points
         XYChart.Series<Number, Number> series2 = new XYChart.Series();
 
-        //csv.raceDB.size()
+        //populates the xy arrays with data
         for (int x = 0; x < csv.raceDB.size(); x++) {
             xs[x] = csv.getRaceID(x);
             ys[x] = csv.getRaceWPM(x);
         }
 
-
+        //fills series1 from the XYAxis element
         for (int x = 0; x < csv.raceDB.size(); x++) {
             series1.getData().add(new XYChart.Data(xs[x], ys[x]));
         }
 
         // calculate the polynomial coefficients and calculate trend points
-        //coefficient - 7, poly - 15
         double[] coefficients = polyfit(xs, ys, 4);
-        System.out.println(coefficients[0]);
 
+        //uses thorwin library to calculate trend line curve
+        //using polynomial formula
         for (double x = 0; x < csv.raceDB.size(); x += 5) {
             double y = polynomial(x, coefficients);
             series2.getData().add(new XYChart.Data<Number, Number>(x,y));
         }
 
-
+        //line chart title
         lineChart.setTitle("ahn94's progress");
+
+        //adds both data series to chart
         lineChart.getData().add(series1);
         lineChart.getData().add(series2);
+
+        //turns off legend & removes the lock to zero on the range of the Y Axis
         lineChart.setLegendVisible(false);
         yAxis.setForceZeroInRange(false);
+
+        //points the line chart to use CSS style chart
         lineChart.getStylesheets().add(DrawChart.class.getResource("style.css").toExternalForm());
 
+        //stackpane container
         StackPane root = new StackPane();
+        //adds linechart to root
         root.getChildren().add(lineChart);
-        Scene scene = new Scene(root, 400, 300);
+        //creates scene & adds the container
+        Scene scene = new Scene(root, 800, 600);
+        //fills stage(stage is complete program window)
         stage.setScene(scene);
+        //makes window visible
         stage.show();
+        //feed back on number of races on chart
         System.out.println(csv.raceDB.size());
     }
 
